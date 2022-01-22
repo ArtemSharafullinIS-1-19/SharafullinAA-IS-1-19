6b33c6a46c6ef56a58cec4b65e6ae213bd0af6d5
s31360 <rejs03@mail.ru>
2022-01-22 08:25:15 +0500

    menu.cs

--- Учебная практика/Program.cs
+++ Учебная практика/Program.cs
@@ -16,7 +16,7 @@ namespace Учебная_практика
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new menu());
         }
     }
 }
