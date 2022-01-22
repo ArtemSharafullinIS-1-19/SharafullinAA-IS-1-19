ba62a96476f78925e4bd266c1ed36c1ab7453b16
s31360 <rejs03@mail.ru>
2022-01-22 09:00:00 +0500

    menu.cs

---
+++ Учебная практика/Form3.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Учебная_практика
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        MySqlConnection conn;
+        string connStr = "server=caseum.ru;port=33333;user=test_user;database=db_test;password=test_pass;";
+        //DataAdapter представляет собой объект Command , получающий данные из источника данных.
+        private MySqlDataAdapter MyDA = new MySqlDataAdapter();
+        //Объявление BindingSource, основная его задача, это обеспечить унифицированный доступ к источнику данных.
+        private BindingSource bSource = new BindingSource();
+        //DataSet - расположенное в оперативной памяти представление данных, обеспечивающее согласованную реляционную программную 
+        //модель независимо от источника данных.DataSet представляет полный набор данных, включая таблицы, содержащие, упорядочивающие 
+        //и ограничивающие данные, а также связи между таблицами.
+        private DataSet ds = new DataSet();
+        //Представляет одну таблицу данных в памяти.
+        private DataTable table = new DataTable();
+        //что бы в БД не отправлялся null
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            conn = new MySqlConnection(connStr);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string commandStr = "SELECT id AS 'Код', fio AS 'ФИО', " +
+                                       "theme_kurs AS 'Тема курсовой' FROM t_stud";
+                //Открываем соединение
+                conn.Open();
+                //Объявляем команду, которая выполнить запрос в соединении conn
+                MyDA.SelectCommand = new MySqlCommand(commandStr, conn);
+                //Заполняем таблицу записями из БД
+                MyDA.Fill(table);
+                //Указываем, что источником данных в bindingsource является заполненная выше таблица
+                bSource.DataSource = table;
+                //Указываем, что источником данных ДатаГрида является bindingsource 
+                dataGridView1.DataSource = bSource;
+                //Закрываем соединение
+                conn.Close();
+            }
+            catch (Exception oshibka)
+            {
+                MessageBox.Show($"{oshibka}");
+            }
+            finally
+            {
+                MessageBox.Show("Вы успешно подключились к базе данных!");
+                conn.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                MessageBox.Show(dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString());
+            }
+            catch
+            {
+            }
+        }
+    }
+}
